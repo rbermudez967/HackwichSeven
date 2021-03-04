a285eb0c93ff6eb09f29967b86eaaaf99666d6bb
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-03-04 09:58:29 -1000

    completed parts 1 - 8

--- HackwichSeven/HackwichSeven/ViewController.swift
+++ HackwichSeven/HackwichSeven/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textLabel.text = " "
     }
 
 
+    @IBAction func setLabelButtonPressed(_ sender: Any)
+    {
+        
+        //create variable that stores user input from text field
+        let userInputText = textField.text
+        
+        //we want to set textLabel to userInputText
+        
+        textLabel.text = userInputText
+        
+        
+    }
+    
+    
 }
 
